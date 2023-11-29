80e5bc47a1dfb9f8c3a70a3bc2917bb25acdf0aa
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2023-11-29 18:08:46 -0500

    minor changes to dockerfile and scripts

--- run_100M_builds.sh
+++ run_100M_builds.sh
@@ -4,13 +4,18 @@
 python3.10 run.py --algorithm ParDiskANN --dataset bigann-100M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParHCNNG --dataset bigann-100M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-100M --definitions artifact_eval.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-100M --definitions artifact_eval.yaml --rebuild
+
 
 #spacev dataset
 python3.10 run.py --algorithm ParDiskANN --dataset msspacev-100M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParHCNNG --dataset msspacev-100M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParPyNNDescent --dataset msspacev-100M --definitions artifact_eval.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset msspacev-100M --definitions artifact_eval.yaml --rebuild
+
 
 #text2image dataset
 python3.10 run.py --algorithm ParDiskANN --dataset text2image-100M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParHCNNG --dataset text2image-100M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParPyNNDescent --dataset text2image-100M --definitions artifact_eval.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset text2image-100M --definitions artifact_eval.yaml --rebuild
--- run_10M_builds.sh
+++ run_10M_builds.sh
@@ -4,13 +4,16 @@
 python3.10 run.py --algorithm ParDiskANN --dataset bigann-10M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParHCNNG --dataset bigann-10M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-10M --definitions artifact_eval.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-10M --definitions artifact_eval.yaml --rebuild
 
 #spacev dataset
 python3.10 run.py --algorithm ParDiskANN --dataset msspacev-10M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParHCNNG --dataset msspacev-10M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParPyNNDescent --dataset msspacev-10M --definitions artifact_eval.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset msspacev-10M --definitions artifact_eval.yaml --rebuild
 
 #text2image dataset
 python3.10 run.py --algorithm ParDiskANN --dataset text2image-10M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParHCNNG --dataset text2image-10M --definitions artifact_eval.yaml --rebuild
 python3.10 run.py --algorithm ParPyNNDescent --dataset text2image-10M --definitions artifact_eval.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset text2image-10M --definitions artifact_eval.yaml --rebuild
