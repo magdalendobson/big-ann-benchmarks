1d86da53ca8ba6594fe751c3778514b297e3fe58
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2024-01-01 10:28:00 -0500

    changes to scripts

--- thread_scaling_bigann.sh
+++ thread_scaling_bigann.sh
@@ -2,56 +2,62 @@
 
 #Comment out any thread counts that your machine does not have
 
-# # builds on 1 thread
-# python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_1.yaml --rebuild
-# python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_1.yaml --rebuild
-# python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_1.yaml --rebuild
-# python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_1.yaml --rebuild
+# builds on 1 thread
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_1.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_1.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_1.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_1.yaml --rebuild
 
-# # builds on 2 threads
-# python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_2.yaml --rebuild
-# python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_2.yaml --rebuild
-# python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_2.yaml --rebuild
-# python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_2.yaml --rebuild
+# builds on 2 threads
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_2.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_2.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_2.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_2.yaml --rebuild
 
-# # builds on 8 threads
-# python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_8.yaml --rebuild
-# python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_8.yaml --rebuild
-# python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_8.yaml --rebuild
-# python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_8.yaml --rebuild
+# builds on 8 threads
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_8.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_8.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_8.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_8.yaml --rebuild
 
-# # builds on 16 threads
-# python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_16.yaml --rebuild
-# python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_16.yaml --rebuild
-# python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_16.yaml --rebuild
-# python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_16.yaml --rebuild
+# builds on 16 threads
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_16.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_16.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_16.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_16.yaml --rebuild
 
-# # builds on 24 threads
-# python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_24.yaml --rebuild
-# python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_24.yaml --rebuild
-# python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_24.yaml --rebuild
-# python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_24.yaml --rebuild
+# builds on 24 threads
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_24.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_24.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_24.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_24.yaml --rebuild
 
-# # builds on 32 threads
-# python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_32.yaml --rebuild
-# python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_32.yaml --rebuild
-# python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_32.yaml --rebuild
-# python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_32.yaml --rebuild
+# builds on 32 threads
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_32.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_32.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_32.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_32.yaml --rebuild
 
-# # builds on 48 threads
-# python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_48.yaml --rebuild
-# python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_48.yaml --rebuild
-# python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_48.yaml --rebuild
-# python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_48.yaml --rebuild
+# builds on 48 threads
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_48.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_48.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_48.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_48.yaml --rebuild
 
-# # builds on 64 threads
-# python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_64.yaml --rebuild
-# python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_64.yaml --rebuild
-# python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_64.yaml --rebuild
-# python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_64.yaml --rebuild
+# builds on 64 threads
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_64.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_64.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_64.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_64.yaml --rebuild
 
 # builds on 96 threads
 python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_96.yaml --rebuild
 python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_96.yaml --rebuild
 python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_96.yaml --rebuild
 python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_96.yaml --rebuild
+
+# builds on 128 threads
+python3.10 run.py --algorithm ParDiskANN --dataset bigann-1M --definitions thread_128.yaml --rebuild
+python3.10 run.py --algorithm ParHCNNG --dataset bigann-1M --definitions thread_128.yaml --rebuild
+python3.10 run.py --algorithm ParPyNNDescent --dataset bigann-1M --definitions thread_128.yaml --rebuild
+python3.10 run.py --algorithm ParHNSW --dataset bigann-1M --definitions thread_128.yaml --rebuild
